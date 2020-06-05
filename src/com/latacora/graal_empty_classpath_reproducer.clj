cf63a5467f23eda86d6cfbfd624d7898e3de9155
Laurens Van Houtven (lvh) <_@lvh.io>
2020-06-05 16:27:20 -0500

    Better reproducer

--- src/com/latacora/graal_empty_classpath_reproducer.clj
+++ src/com/latacora/graal_empty_classpath_reproducer.clj
@@ -1,24 +1,32 @@
 (ns com.latacora.graal-empty-classpath-reproducer
   (:require
    [clojure.java.classpath :as cp]
-   [clojure.pprint :as pp]
-   [clojure.string :as str])
+   [clojure.pprint :as pp])
   (:gen-class))
 
 (defn -main
   "Print classpaths, computed two ways, plus their length"
   [& args]
   (let [alleged-classpath (cp/classpath)
-        property-classpath (str/split
-                            (System/getProperty "java.class.path")
-                            (re-pattern (System/getProperty "path.separator")))]
+        system-classpath (cp/system-classpath)]
     (pp/pprint
-     {:java-home (System/getProperty "java.home")
+     {;; while these don't always have to be equal, even for a non-buggy impl,
+      ;; this is the simplest way to see if the bug is occurring in our test.
+      :buggy? (not= alleged-classpath system-classpath)
+
+      :java-home (System/getProperty "java.home")
       :java-vendor (System/getProperty "java.vendor")
       :java-version (System/getProperty "java.version")
 
       :alleged-classpath-length (count alleged-classpath)
-      :alleged-classpath (vec alleged-classpath)
+      :alleged-classpath (if (-> alleged-classpath count (= 1))
+                           (vec alleged-classpath)
+                           :too-long)
 
-      :property-classpath-length (count property-classpath)
-      :property-classpath (vec property-classpath)})))
+      :whence-alleged-classpath (->> (clojure.lang.RT/baseLoader)
+                                     (iterate #(.getParent ^ClassLoader %))
+                                     (take-while identity)
+                                     (mapv (juxt identity cp/loader-classpath)))
+
+      :property-classpath-length (count system-classpath)
+      :property-classpath (vec system-classpath)})))
